3f527ab3fc43cc7bc82b26e32438d4a9c3ebaa90
joneff <joneff@gmail.com>
2019-09-29 21:22:47 +0300

    chore: add eslint and editorconfig
    * fix lint errors

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const reImport = /^\/\/ #= (import|import_once) (.*?)$/gm;
 const importedFiles = new Set();
 const importedOnceFiles = new Set();
 let currentPath;
-const process_cwd = process.cwd();
+const process_cwd = process.cwd(); // eslint-disable-line camelcase
 
 
 const parse = (url, root = false) => {
-    const buffer = fs.readFileSync(url, "utf8");
-    let output = "";
+    const buffer = fs.readFileSync(url, 'utf8');
+    let output = '';
 
     currentPath = path.dirname( url );
     importedFiles.add( url );
 
     if (root === true) {
         output += [
-            "// This file is auto-generated. Do not edit!",
-            `// Origin .${path.dirname( url ).replace(process_cwd, "").replace(/\\/g, "/")}/${path.basename( url )}`,
-            "\n"
-        ].join("\n");
+            '// This file is auto-generated. Do not edit!',
+            `// Origin .${path.dirname( url ).replace(process_cwd, '').replace(/\\/g, '/')}/${path.basename( url )}`,
+            '\n'
+        ].join('\n');
     }
 
     output += buffer.replace(reImport, importReplacer);
 
     return output;
-}
+};
 
-const importReplacer = (match, import_type, file, offset, input) => {
+const importReplacer = (match, importType, file) => {
     let output = [];
     const toBeImportedFile = path.join( currentPath, file );
 
     output.push(match);
 
-    output.push(`\/\/ #region ${import_type} .${currentPath.replace(process_cwd, "").replace(/\\/g, "/")}/${file}`);
+    output.push(`// #region ${importType} .${currentPath.replace(process_cwd, '').replace(/\\/g, '/')}/${file}`);
 
     output.push(
         (
             importedOnceFiles.has(toBeImportedFile)
             ||
-            (import_type == "import_once" && importedFiles.has(toBeImportedFile))
+            (importType === 'import_once' && importedFiles.has(toBeImportedFile))
         )
-        ? "\/\/ File already imported_once. Skipping output."
-        : parse( toBeImportedFile )
+            ? '// File already imported_once. Skipping output.'
+            : parse( toBeImportedFile )
     );
 
-    output.push(`\/\/ #endregion`);
+    output.push('// #endregion');
 
-    if (import_type === "import_once") {
+    if (importType === 'import_once') {
         importedOnceFiles.add(toBeImportedFile);
     }
 
-    return output.join("\n");
-}
+    return output.join('\n');
+};
 
-const compile = (file, outFile) => {
+const compile = (file, outFile) => { // eslint-disable-line consistent-return
     const output = parse(file, true);
 
     if (outFile) {
         try {
             fs.accessSync(outFile, fs.constants.W_OK | fs.constants.R_OK);
-        } catch(error) {
+        } catch (error) {
             if (error.code !== 'ENOENT') {
-                console.error(error);
+                console.error(error); // eslint-disable-line no-console
                 process.exit(1);
             }
         }
-        console.info(`Inlineing ${file} to ${outFile}`);
+        console.info(`Inlineing ${file} to ${outFile}`); // eslint-disable-line no-console
         fs.writeFileSync(outFile, output);
     } else {
         return output;
     }
-}
+};
 
 
 module.exports = {
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
-const path = require("path");
-const baka = require("../index.js");
+const path = require('path');
+const baka = require('../index.js');
 
-const base = path.join(__dirname, "scss");
+const base = path.join(__dirname, 'scss');
 
-const file = path.join(base, "index.scss");
-const outFile = path.join(base, "all.scss");
+const file = path.join(base, 'index.scss');
+// const outFile = path.join(base, 'all.scss');
 
 baka.compile(file);
