7e67fa66eb7682902515dfb4b9cbab2503d30a02
joneff <joneff@gmail.com>
2021-07-27 19:39:35 +0300

    chore: cleanup a few more dependencies

--- test/buildSass.js
+++ test/buildSass.js
@@ -1,35 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const calc = require('postcss-calc');
 const sassImporterFactory = require('./sassimporter');
 
-const browsers = [
-    'last 1 Edge versions',
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions'
-];
-const postcssPlugins = [
-    calc({
-        precision: 10
-    }),
-    autoprefixer({
-        overrideBrowserslist: browsers
-    })
-];
 const sassOptions = {
     precision: 10,
     outputStyle: 'expanded',
     importer: sassImporterFactory({ cache: true, cwd: __dirname })
 };
+
 const buildSass = async (src, dest) => { // eslint-disable-line space-before-function-paren
 
     return new Promise((resolve) => {
         gulp.src(src)
             .pipe(sass(sassOptions).on('error', sass.logError))
-            .pipe(postcss(postcssPlugins))
             .pipe(gulp.dest(dest))
             .on('end', () => {
                 resolve();
