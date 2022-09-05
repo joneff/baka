b7cd0d4dbec72b2f267fd1bb9cf0796cc1f24ce4
joneff <joneff@gmail.com>
2022-09-05 22:10:44 +0300

    test: change mocha to jest

--- __tests__/foundation.test.js
+++ __tests__/foundation.test.js
@@ -1,31 +1,27 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const assert = require('assert');
-const suite = require('mocha').suite;
-const before = require('mocha').before;
-const context = require('mocha').describe;
-const test = require('mocha').test;
-const { del, buildSass } = require('./index');
+
 const baka = require('../index');
+const { mkdir, del, sassBuild } = require('../lib/utils');
 
-const node_modules = path.join(__dirname, 'node_modules'); // eslint-disable-line camelcase
 const dist = path.join(__dirname, 'dist');
-const local = path.join(__dirname, 'local');
+const fixtures = path.join(__dirname, '__fixtures__');
 
 
-suite('foundation', () => {
+describe('foundation', () => {
 
-    before(() => {
-        fs.mkdirpSync(dist);
+    beforeAll(() => {
+        mkdir(dist);
     });
 
-    context('baka:full', () => {
+    describe('baka:full', () => {
 
-        test('source vs flat', async () => { // eslint-disable-line space-before-function-paren
-            let src = path.join(node_modules, 'foundation-sites/scss/foundation.scss');
+        test('source vs flat', () => {
+            let src = path.join('node_modules', 'foundation-sites/scss/foundation.scss');
             let flat = path.join(dist, 'foundation-flat.scss');
-            let _src = path.join(local, 'foundation.scss');
-            let _flat = path.join(local, 'foundation-flat.scss');
+            let _src = path.join(fixtures, 'foundation.scss');
+            let _flat = path.join(fixtures, 'foundation-flat.scss');
             let _srcOut = path.join(dist, 'foundation.css');
             let _flatOut = path.join(dist, 'foundation-flat.css');
 
@@ -33,10 +29,8 @@ suite('foundation', () => {
 
             baka.build({ file: src, output: { path: dist } });
 
-            await Promise.all([
-                buildSass(_src, dist),
-                buildSass(_flat, dist)
-            ]);
+            sassBuild(_src, _srcOut);
+            sassBuild(_flat, _flatOut);
 
             assert.strictEqual( fs.readFileSync(_srcOut, 'utf-8').length, fs.readFileSync(_flatOut, 'utf-8').length );
         });
--- __tests__/materialize.test.js
+++ __tests__/materialize.test.js
@@ -1,31 +1,27 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
 const assert = require('assert');
-const suite = require('mocha').suite;
-const before = require('mocha').before;
-const context = require('mocha').describe;
-const test = require('mocha').test;
-const { del, buildSass } = require('./index');
+
 const baka = require('../index');
+const { mkdir, del, sassBuild } = require('../lib/utils');
 
-const node_modules = path.join(__dirname, 'node_modules'); // eslint-disable-line camelcase
 const dist = path.join(__dirname, 'dist');
-const local = path.join(__dirname, 'local');
+const fixtures = path.join(__dirname, '__fixtures__');
 
 
-suite('materialize', () => {
+describe('materialize', () => {
 
-    before(() => {
-        fs.mkdirpSync(dist);
+    beforeAll(() => {
+        mkdir(dist);
     });
 
-    context('baka:full', () => {
+    describe('baka:full', () => {
 
-        test('source vs flat', async () => { // eslint-disable-line space-before-function-paren
-            let src = path.join(node_modules, 'materialize-css/sass/materialize.scss');
+        test('source vs flat', () => {
+            let src = path.join('node_modules', 'materialize-css/sass/materialize.scss');
             let flat = path.join(dist, 'materialize-flat.scss');
-            let _src = path.join(local, 'materialize.scss');
-            let _flat = path.join(local, 'materialize-flat.scss');
+            let _src = path.join(fixtures, 'materialize.scss');
+            let _flat = path.join(fixtures, 'materialize-flat.scss');
             let _srcOut = path.join(dist, 'materialize.css');
             let _flatOut = path.join(dist, 'materialize-flat.css');
 
@@ -33,10 +29,8 @@ suite('materialize', () => {
 
             baka.build({ file: src, output: { path: dist } });
 
-            await Promise.all([
-                buildSass(_src, dist),
-                buildSass(_flat, dist)
-            ]);
+            sassBuild(_src, _srcOut);
+            sassBuild(_flat, _flatOut);
 
             assert.strictEqual( fs.readFileSync(_srcOut, 'utf-8').length, fs.readFileSync(_flatOut, 'utf-8').length );
         });
